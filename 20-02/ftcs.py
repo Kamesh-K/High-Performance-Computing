b6f0266177b834acd1c02fff5a4bc41bea610116
Kamesh-K <kamesh3799@gmail.com>
2020-03-24 17:44:59 +0530

    Typesetting the codes

--- 20-02/ftcs.py
+++ 20-02/ftcs.py
@@ -1,8 +1,8 @@
 # Program to solve the 1-D transient heat equation
-# Importing the required libraries 
+# Importing the required libraries
 import math
-import random 
-import numpy as np	
+import random
+import numpy as np
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -10,66 +10,73 @@ size = comm.Get_size()
 n = size
 # Declaring the conditions of the system
 del_x = 0.01
-del_t = 0.01 
+del_t = 0.01
 length = 1
 time_max = 100
 alpha = 1.11 * 10**(-4)
-size_list = int(length/del_x) 
+size_list = int(length / del_x)
 temp_arr = None
-block_size = int(size_list/size)
+block_size = int(size_list / size)
 local_temp_arr = np.empty(block_size).fill(0)
-# Initializing the temperature array for 1-D 
+# Initializing the temperature array for 1-D
 if rank == 0:
     temp_arr = np.empty([size, block_size], dtype='f')
-    temp_arr.T[:,:] = 300
-    temp_arr[size-1,block_size-1]=500
+    temp_arr.T[:, :] = 300
+    temp_arr[size - 1, block_size - 1] = 500
 local_temp_arr = np.empty(block_size, dtype='f')
-# Scattering the array to different processes to do computations in those processes 
+# Scattering the array to different processes to do computations in those
+# processes
 comm.Scatter(temp_arr, local_temp_arr, root=0)
 temp_1 = local_temp_arr
 temp_2 = local_temp_arr
-# Declaring and computing the constants for calculations 
-r = alpha * del_t/(del_x**2)
-max_time_step = int(time_max/del_t)
-time_step = 0 
-# Declaring elements which are received from other processes for computing the temperature evolution
-left_elem = None 
-right_elem = None 
+# Declaring and computing the constants for calculations
+r = alpha * del_t / (del_x**2)
+max_time_step = int(time_max / del_t)
+time_step = 0
+# Declaring elements which are received from other processes for computing
+# the temperature evolution
+left_elem = None
+right_elem = None
 # Declaring the file to dump the temperature evolution
-open('Temperature_Profile.txt', 'w').close()	
+open('Temperature_Profile.txt', 'w').close()
 # Running the time loop for maximum limit set by the user
 while time_step < max_time_step:
-	# All the itermediate values in a sub domain is calculated for which no data is required to be exchanged from the nearby processes
-	for i in range(1,block_size-1):
-		temp_2[i]= temp_1[i] + r*(temp_1[i+1] - 2*temp_1[i] + temp_1[i-1])
-	# For rank = 0, we need to receive only one data from rank = 1 for the final element in rank 0 
-	if rank == 0:
-		comm.send(temp_1[block_size-1], dest = rank+1, tag = time_step)
-		right_elem = comm.recv(source = rank+1,tag = time_step)
-		temp_2[block_size-1] = temp_1[block_size-1] + r*(right_elem - 2*temp_1[block_size-1]+ temp_1[block_size-2])
-	# For rank = size - 1, we need to receive only one data from rank = size - 2 for the final element in rank size - 1 
-	elif rank == size - 1:
-		comm.send(temp_1[0],dest = rank-1, tag = time_step)
-		left_elem = comm.recv(source = rank -1, tag = time_step)
-		temp_2[0] = temp_1[0] + r*(temp_1[1] - 2*temp_1[0] + left_elem)
-	# For all intermediate ranks between rank 0 and rank size-1, we will have to receive two elements 
-	# One from the rank-1 process and one from rank+1 process 
-	# Suitable equations are written so that this is take care of 
-	else:
-		comm.send(temp_1[block_size-1], dest = rank+1, tag = time_step)
-		comm.send(temp_1[0],dest = rank-1, tag = time_step)
-		left_elem = comm.recv(source = rank -1, tag = time_step)
-		right_elem = comm.recv(source = rank+1,tag = time_step)
-		temp_2[block_size-1] = temp_1[block_size-1] + r*(right_elem - 2*temp_1[block_size-1]+ temp_1[block_size-2])
-		temp_2[0] = temp_1[0] + r*(temp_1[1] - 2*temp_1[0] + left_elem)
-	temp_1 = temp_2 
-	time_step += 1	# Incrementing the time iter and looping continues 
-	# Gather all the computed temperature profile and place it in temp_arr
-	comm.Gather(temp_1,temp_arr,root =0)
+    # All the itermediate values in a sub domain is calculated for which no
+    # data is required to be exchanged from the nearby processes
+    for i in range(1, block_size - 1):
+        temp_2[i] = temp_1[i] + r * \
+            (temp_1[i + 1] - 2 * temp_1[i] + temp_1[i - 1])
+    # For rank = 0, we need to receive only one data from rank = 1 for the
+    # final element in rank 0
+    if rank == 0:
+        comm.send(temp_1[block_size - 1], dest=rank + 1, tag=time_step)
+        right_elem = comm.recv(source=rank + 1, tag=time_step)
+        temp_2[block_size - 1] = temp_1[block_size - 1] + r * \
+            (right_elem - 2 * temp_1[block_size - 1] + temp_1[block_size - 2])
+    # For rank = size - 1, we need to receive only one data from rank = size -
+    # 2 for the final element in rank size - 1
+    elif rank == size - 1:
+        comm.send(temp_1[0], dest=rank - 1, tag=time_step)
+        left_elem = comm.recv(source=rank - 1, tag=time_step)
+        temp_2[0] = temp_1[0] + r * (temp_1[1] - 2 * temp_1[0] + left_elem)
+    # For all intermediate ranks between rank 0 and rank size-1, we will have to receive two elements
+    # One from the rank-1 process and one from rank+1 process
+    # Suitable equations are written so that this is take care of
+    else:
+        comm.send(temp_1[block_size - 1], dest=rank + 1, tag=time_step)
+        comm.send(temp_1[0], dest=rank - 1, tag=time_step)
+        left_elem = comm.recv(source=rank - 1, tag=time_step)
+        right_elem = comm.recv(source=rank + 1, tag=time_step)
+        temp_2[block_size - 1] = temp_1[block_size - 1] + r * \
+            (right_elem - 2 * temp_1[block_size - 1] + temp_1[block_size - 2])
+        temp_2[0] = temp_1[0] + r * (temp_1[1] - 2 * temp_1[0] + left_elem)
+    temp_1 = temp_2
+    time_step += 1  # Incrementing the time iter and looping continues
+    # Gather all the computed temperature profile and place it in temp_arr
+    comm.Gather(temp_1, temp_arr, root=0)
 
-	if rank ==0:
-		# Dump the temp_arr in the file and keep appending till the loop runs
-		temp_csv = np.reshape(temp_arr,(1,size_list))
-		with open("Temperature_Profile.txt", "a") as myfile:
-		 	np.savetxt(myfile, temp_csv, fmt='%1.4e', delimiter=",")
-
+    if rank == 0:
+        # Dump the temp_arr in the file and keep appending till the loop runs
+        temp_csv = np.reshape(temp_arr, (1, size_list))
+        with open("Temperature_Profile.txt", "a") as myfile:
+            np.savetxt(myfile, temp_csv, fmt='%1.4e', delimiter=",")
--- 28-02/Random_walk.py
+++ 28-02/Random_walk.py
@@ -1,96 +1,123 @@
 # Program to simulate a random walk process using Reflective boundary condition
-# Importing the required libraries 
+# Importing the required libraries
 import math
-import random 
-import numpy as np	
+import random
+import numpy as np
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-# Declaing the variables used in the code 
+# Declaing the variables used in the code
 nProcs = comm.Get_size()
 n = nProcs
 P = 24
-del_x = 0.4 	# Step size used for computation 
+del_x = 0.4 	# Step size used for computation
 random.seed(2)
+
+
 class particle:
-	pId = None 
-	loc = 0
-	def __init__(self,pId,loc):
-		self.pId = pId 
-		self.loc = loc 
-pList = []		# Declaring the list of particles for the process 
-nParticles = int(P/nProcs) # Number of particles present in the process 
-# Initializing the particles with an ID and location 
+    pId = None
+    loc = 0
+
+    def __init__(self, pId, loc):
+        self.pId = pId
+        self.loc = loc
+pList = []		# Declaring the list of particles for the process
+nParticles = int(P / nProcs)  # Number of particles present in the process
+# Initializing the particles with an ID and location
 for i in range(nParticles):
-	loc = np.random.random()
-	pList.append(particle(i+rank*nParticles,loc))
-# Declaring the number of iterations to which the random walk is to be carried out 
+    loc = np.random.random()
+    pList.append(particle(i + rank * nParticles, loc))
+# Declaring the number of iterations to which the random walk is to be
+# carried out
 iter = 1
 iter_max = 100
-# Displaying the number of particles present in each process before random walk 
-print("Number of particles in rank {} = {}").format(rank,len(pList))
-open('Particle_Track.txt', 'w').close()	
+# Displaying the number of particles present in each process before random walk
+print("Number of particles in rank {} = {}").format(rank, len(pList))
+open('Particle_Track.txt', 'w').close()
 # Iteration for random walk begins
 while iter <= iter_max:
-	# Declaring the number for particles to be shifted to left and right process 
-	particle_left = 0
-	particle_right = 0
-	nParticles = len(pList)
-	# Discplacing the particles by a random number 
-	for i in range(nParticles):
-		displace = (np.random.random()-0.5)*del_x
-		pList[i].loc += displace
-	pop_list = []
-	# Checking whether the particles are still in the range of the process domain 
-	for i in range(len(pList)):
-		if pList[i].loc <0:
-			if rank == 0:
-				pList[i].loc = -1*pList[i].loc 	# Reflective boundary condition 
-			else: 
-				particle_left+=1				# Incrementing particle to be shifted to left process by 1 
-				pList[i].loc = 1-pList[i].loc 	# Initializing the new location in the left process 
-				comm.send(pList[i],dest = rank-1, tag = (iter+2)**2) # Sending the particle to the left process 
-				pop_list.append(i-len(pop_list))# Appending the pop list to remove from the current process 
-		elif pList[i].loc >1:
-			if rank==nProcs-1:
-				pList[i].loc = 1 - pList[i].loc # Reflective boundary condition 
-			else:
-				particle_right+=1;
-				pList[i].loc = pList[i].loc - 1 # Initializing the new location in the right process 
-				comm.send(pList[i],dest = rank+1, tag = (iter+2)**2) # Sending the particle to the right process 
-				pop_list.append(i-len(pop_list))# Appending the pop list to remove from the current process 
-	for i in range(len(pop_list)):
-		pList.pop(pop_list[i])					# Popping the data from the list 
+    # Declaring the number for particles to be shifted to left and right
+    # process
+    particle_left = 0
+    particle_right = 0
+    nParticles = len(pList)
+    # Discplacing the particles by a random number
+    for i in range(nParticles):
+        displace = (np.random.random() - 0.5) * del_x
+        pList[i].loc += displace
+    pop_list = []
+    # Checking whether the particles are still in the range of the process
+    # domain
+    for i in range(len(pList)):
+        if pList[i].loc < 0:
+            if rank == 0:
+                # Reflective boundary condition
+                pList[i].loc = -1 * pList[i].loc
+            else:
+                particle_left += 1				# Incrementing particle to be shifted to left process by 1
+                # Initializing the new location in the left process
+                pList[i].loc = 1 - pList[i].loc
+                # Sending the particle to the left process
+                comm.send(pList[i], dest=rank - 1, tag=(iter + 2)**2)
+                # Appending the pop list to remove from the current process
+                pop_list.append(i - len(pop_list))
+        elif pList[i].loc > 1:
+            if rank == nProcs - 1:
+                # Reflective boundary condition
+                pList[i].loc = 1 - pList[i].loc
+            else:
+                particle_right += 1
+                # Initializing the new location in the right process
+                pList[i].loc = pList[i].loc - 1
+                # Sending the particle to the right process
+                comm.send(pList[i], dest=rank + 1, tag=(iter + 2)**2)
+                # Appending the pop list to remove from the current process
+                pop_list.append(i - len(pop_list))
+    for i in range(len(pop_list)):
+        pList.pop(pop_list[i])					# Popping the data from the list
 
-	if rank == 0:
-		comm.send(particle_right,dest = rank+1, tag = iter)						# Sending the number of particles to be sent to left process 
-		particle_left = comm.recv(source = rank+1,tag = iter)					# Receiving number of particles to be received
-		for i in range(particle_left):
-			shifted_particle = comm.recv(source = rank+1,tag = (iter+2)**2)		# Sending the number of particle to right process
-			pList.append(shifted_particle)										# Appending to the current particle list of process 
-	elif rank == nProcs-1:
-		comm.send(particle_left,dest=rank-1,tag = iter)							# Sending the number of particles to be sent to left process
-		particle_right = comm.recv(source = rank-1,tag = iter)					# Receiving number of particles to be received
-		for i in range(particle_right):
-			shifted_particle = comm.recv(source = rank-1,tag = (iter+2)**2)
-			pList.append(shifted_particle)
-	else:
-		comm.send(particle_right,dest = rank+1, tag = iter)						# Sending the number of particles to be sent to right process
-		comm.send(particle_left,dest=rank-1,tag = iter)							# Sending the number of particles to be sent to left process
-		particle_left = comm.recv(source = rank+1,tag = iter)
-		for i in range(particle_left):
-			shifted_particle = comm.recv(source = rank+1,tag = (iter+2)**2)		# Receiving number of particles to be received
-			pList.append(shifted_particle)										# Appending to the current particle list of process 
-		particle_right = comm.recv(source = rank-1,tag = iter)
-		for i in range(particle_right):
-			shifted_particle = comm.recv(source = rank-1,tag = (iter+2)**2)		# Receiving number of particles to be received
-			pList.append(shifted_particle)										# Appending to the current particle list of process 
-	iter+=1
-	particle_found = 0
-	nParticles = len(pList)						# Reinitializing the size of Particle list 
-	for i in range(nParticles):
-		if pList[i].pId == 0:
-			with open('Particle_Track.txt', 'a') as f:
-				print >> f, float((pList[i].loc)+float(rank))					# Writing the tracked value into the file
-# Displaying the number of particles present in each process before random walk 				
-print("Number of particles in rank {} = {}").format(rank,len(pList))	
+    if rank == 0:
+        # Sending the number of particles to be sent to left process
+        comm.send(particle_right, dest=rank + 1, tag=iter)
+        # Receiving number of particles to be received
+        particle_left = comm.recv(source=rank + 1, tag=iter)
+        for i in range(particle_left):
+            # Sending the number of particle to right process
+            shifted_particle = comm.recv(source=rank + 1, tag=(iter + 2)**2)
+            # Appending to the current particle list of process
+            pList.append(shifted_particle)
+    elif rank == nProcs - 1:
+        # Sending the number of particles to be sent to left process
+        comm.send(particle_left, dest=rank - 1, tag=iter)
+        # Receiving number of particles to be received
+        particle_right = comm.recv(source=rank - 1, tag=iter)
+        for i in range(particle_right):
+            shifted_particle = comm.recv(source=rank - 1, tag=(iter + 2)**2)
+            pList.append(shifted_particle)
+    else:
+        # Sending the number of particles to be sent to right process
+        comm.send(particle_right, dest=rank + 1, tag=iter)
+        # Sending the number of particles to be sent to left process
+        comm.send(particle_left, dest=rank - 1, tag=iter)
+        particle_left = comm.recv(source=rank + 1, tag=iter)
+        for i in range(particle_left):
+            # Receiving number of particles to be received
+            shifted_particle = comm.recv(source=rank + 1, tag=(iter + 2)**2)
+            # Appending to the current particle list of process
+            pList.append(shifted_particle)
+        particle_right = comm.recv(source=rank - 1, tag=iter)
+        for i in range(particle_right):
+            # Receiving number of particles to be received
+            shifted_particle = comm.recv(source=rank - 1, tag=(iter + 2)**2)
+            # Appending to the current particle list of process
+            pList.append(shifted_particle)
+    iter += 1
+    particle_found = 0
+    nParticles = len(pList)						# Reinitializing the size of Particle list
+    for i in range(nParticles):
+        if pList[i].pId == 0:
+            with open('Particle_Track.txt', 'a') as f:
+                # Writing the tracked value into the file
+                print >> f, float((pList[i].loc) + float(rank))
+# Displaying the number of particles present in each process before random walk
+print("Number of particles in rank {} = {}").format(rank, len(pList))
